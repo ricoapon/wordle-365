45c302e4d56587e835a95faf45b5d0b2099c0b62
Rico Apon <rico.apon@gmail.com>
2025-01-04 20:18:00 +0100

    Add version to site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
+import {VERSION} from "../version";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,10 @@ import {RouterLink, RouterOutlet} from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'wordle-365';
+
+  ngOnInit(): void {
+    console.log(VERSION)
+  }
 }
---
+++ src/version.ts
@@ -0,0 +1,3 @@
+// This file is updated when the product is built for production. If you change the structure of this file, make sure to
+// change build-for-gh-pages.sh as well!
+export const VERSION = 'd.e.v';
