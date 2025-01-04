7bded7c9ce477bfaf027c8012b4831d0546a5887
Rico Apon <rico.apon@gmail.com>
2025-01-04 22:23:39 +0100

    Add Date util service

---
+++ src/app/backend/date-util-service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {DatePipe} from "@angular/common";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DateUtilService {
+
+  constructor(private datePipe: DatePipe) {
+  }
+
+  convertDateToIsoFormatString(date: Date): string {
+    const result = this.datePipe.transform(date, 'yyyy-MM-dd');
+    if (result == null) {
+      throw new Error('Error converting date to iso format: ' + date);
+    }
+    return result;
+  }
+}
