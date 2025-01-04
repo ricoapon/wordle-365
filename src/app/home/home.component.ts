3abe71f766320dcbd864e1e6534f75315f61e196
Rico Apon <rico.apon@gmail.com>
2025-01-04 23:00:23 +0100

    Show played games on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,27 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink} from "@angular/router";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {StorageService} from "../backend/storage-service";
+import {WordleSingleDay} from "../backend/wordle-single-day";
+import {DateUtilService} from "../backend/date-util-service";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     RouterLink,
-    NgForOf
+    NgForOf,
+    NgIf
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public dataWithoutToday: WordleSingleDay[]
+  public today: WordleSingleDay | undefined;
+  private readonly todayDate: String;
 
-  constructor(storageService: StorageService) {
-    console.log(storageService.getAll());
+  constructor(private storageService: StorageService, dateUtilService: DateUtilService) {
+    this.todayDate = dateUtilService.today();
   }
 
-  public yesterday(): String {
-    const date = new Date();
-    date.setDate(date.getDate() - 1);
-    return date.toISOString().split('T')[0]
+  ngOnInit(): void {
+    this.today = this.storageService.getForDate(this.todayDate);
+    // We reverse the data, so that the newest games are always shown on top.
+    this.dataWithoutToday = this.storageService.getAll()
+      .reverse()
+      .filter(w => w != this.today)
   }
 }
