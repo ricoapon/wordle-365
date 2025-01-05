a23add544d7d5d120cc3ca81f5d7017ea51539dd
Rico Apon <rico.apon@gmail.com>
2025-01-05 16:19:50 +0100

    Make animations possible

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
 import {DatePipe} from "@angular/common";
 import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     DatePipe,
     provideHttpClient(withInterceptorsFromDi()),
-  ],
+    importProvidersFrom([BrowserAnimationsModule]),
+  ]
 };
